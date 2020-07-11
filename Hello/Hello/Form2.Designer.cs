b1456e576554c7b84be7489c77125aab6fa788c7
shashanks4 <49721667+shashanks4@users.noreply.github.com>
2020-07-11 22:28:41 +0530

    Add files via upload

---
+++ Hello/Hello/Form2.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Hello
+{
+	partial class Form2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.button1 = new System.Windows.Forms.Button();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.hiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.Visible = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(339, 134);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(89, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Hello";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hiToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(85, 26);
+			// 
+			// hiToolStripMenuItem
+			// 
+			this.hiToolStripMenuItem.Name = "hiToolStripMenuItem";
+			this.hiToolStripMenuItem.Size = new System.Drawing.Size(84, 22);
+			this.hiToolStripMenuItem.Text = "hi";
+			// 
+			// Form2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button1);
+			this.Name = "Form2";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form2";
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem hiToolStripMenuItem;
+	}
+}
